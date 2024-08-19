b91e73888ad8d55fb1b484b89dc32e5dcab2add2
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-08-19 13:58:10 -0600

    rutas

--- proyectofinal/lib/main.dart
+++ proyectofinal/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:proyectofinal/pages/inicio_page.dart';
+import 'package:proyectofinal/pages/login_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,9 +9,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StartPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const StartPage(),
+        '/login': (context) => LoginPage(), 
+
+      },
     );
   }
 }
