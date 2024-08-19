6fe5d60690c5bea64004876b57bd2c035b300881
eventuraa15 <141580602+Eduar1505@users.noreply.github.com>
2024-08-19 13:58:58 -0600

    login sin validacion

--- proyectofinal/lib/pages/login_page.dart
+++ proyectofinal/lib/pages/login_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  LoginPage({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller : emailController,
+                decoration: const InputDecoration(labelText: 'Correo Electronico'),
+              ),
+
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Contraseña'),
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 20,),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: const Text('Iniciar Sesion')
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/register');
+                },
+                child: const Text('Registrarse'))
+
+            ],
+          ),
+      ),
+    );
   }
 }
